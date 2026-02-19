7ca85f45b939a157df874bf8afeaa16b67b62619
Anas Benalla <iecxnre@gmail.com>
2026-02-19 12:42:33 +0000

    Refactor: Update Flux menu component tags in sidebar for consistency and improved readability

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -122,13 +122,13 @@
                                 </div>
                             </div>
                         </div>
-                    </flux:menu.radio-group>
+                    </flux:menu.radio.group>
 
                     <flux:menu.separator />
 
-                    <flux:menu.radio-group>
+                    <flux:menu.radio.group>
                         <flux:menu.item :href="route('profile.edit')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-                    </flux:menu.radio-group>
+                    </flux:menu.radio.group>
 
                     <flux:menu.separator />
 
