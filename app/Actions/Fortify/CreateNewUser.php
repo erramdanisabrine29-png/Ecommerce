f5ba268f819de78ca26663860d9d673da2e36ae3
Sabrine <erramdanisabrine29@gmail.com>
2026-03-03 00:03:34 +0100

    Fix database connection, models, controllers, and ensure users/stores are saved correctly

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -33,7 +33,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
-            'password' => $input['password'],
+            'password' => bcrypt($input['password']),
         ]);
     }
 }
